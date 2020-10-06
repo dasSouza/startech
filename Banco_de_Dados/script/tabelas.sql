388ec9764c70699aa87b85cbfe01bade3fa55a59
Gustavo Cassimiro <68086908+cKassimiro@users.noreply.github.com>
2020-10-05 22:22:02 -0300

    Update tabelas.sql

--- Banco_de_Dados/script/tabelas.sql
+++ Banco_de_Dados/script/tabelas.sql
@@ -5,9 +5,9 @@ CREATE TABLE tbUsuario (
 	id int primary key auto_increment,
 	nome varchar(64) not null,
 	email varchar(100) not null,
-    cidade varchar(50) not null,
-    UF char(2) not null,
-    telefone varchar(14) not null,
+    	cidade varchar(50) not null,
+    	UF char(2) not null,
+    	telefone varchar(14) not null,
 	horario_cadastro datetime not null
 	) auto_increment = 1000;
                         
@@ -16,7 +16,7 @@ CREATE TABLE tbLogin_Usuario (
 	usuario varchar(32) not null,
 	senha varchar(64) not null,
 	horario_login datetime not null,
-    fk_id int,
-    foreign key (fk_id) references tbUsuario(id)
+    	fk_id int,
+    	foreign key (fk_id) references tbUsuario(id)
 	);
                               
