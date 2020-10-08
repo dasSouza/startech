fe77a49444f41fe2a3b8d6f912cab252586cd415
cKassimiro <gustavo.oliveira@bandtec.com.br>
2020-10-08 20:21:50 -0300

    att banco / att fk

--- Projeto Site/Banco_de_Dados/script/selects.sql
+++ Projeto Site/Banco_de_Dados/script/selects.sql
@@ -4,4 +4,4 @@ SELECT * FROM tbUsuario;
 
 SELECT * FROM tbUsuario, tbLogin_Usuario where fk_id_Usuario = id_Usuario;
 
-SELECT * FROM tbSensores, tbDados_Sensores where fk_id_dados = id_dados;
+SELECT * FROM tbSensores, tbDados_Sensores where fk_id_sensores = id_dados;
--- Projeto Site/Banco_de_Dados/script/tabelas.sql
+++ Projeto Site/Banco_de_Dados/script/tabelas.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE BD_init_0;
 USE BD_init_0;
 
+-- 
 CREATE TABLE tbUsuario (
 	id_Usuario int primary key auto_increment,
 	nome varchar(64) not null,
@@ -20,21 +21,22 @@ CREATE TABLE tbLogin_Usuario (
     	foreign key (fk_id_Usuario) references tbUsuario(id_Usuario)
 );
 
-CREATE TABLE tbDados_Sensores (
-  id_dados INT PRIMARY KEY AUTO_INCREMENT,
-  temperatura DECIMAL(5,2) NOT NULL,
-  umidade DECIMAL(5,2) NOT NULL,
-  horario_captacao DATETIME NOT NULL
-);
 
 CREATE TABLE tbSensores (
   id_sensor INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(5) NOT NULL,
   zona_area ENUM('norte', 'sul', 'leste', 'oeste', 'centro') NOT NULL,
   estado_sensor BOOLEAN DEFAULT TRUE,
-  horario_primeiro_uso DATETIME NOT NULL,
-	fk_id_dados int,
-    foreign key (fk_id_dados) references tbDados_Sensores(id_dados)
+  horario_primeiro_uso DATETIME NOT NULL
+);
+
+CREATE TABLE tbDados_Sensores (
+  id_dados INT PRIMARY KEY AUTO_INCREMENT,
+  temperatura DECIMAL(5,2) NOT NULL,
+  umidade DECIMAL(5,2) NOT NULL,
+  horario_captacao DATETIME NOT NULL,
+	fk_id_sensores int,
+	foreign key (fk_id_sensores) references tbSensores(id_sensor)
 );
 
 describe tbDados_Sensores;
