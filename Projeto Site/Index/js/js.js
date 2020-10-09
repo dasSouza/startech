d12d233051799b5c5cc2961a5b5877c3c490c6e0
Ju-Souza <ju.silva@bandtec.com.br>
2020-10-08 21:01:07 -0300

    primeira versao do site

---
+++ Projeto Site/Index/js/js.js
@@ -0,0 +1,8 @@
+function openmenu(){
+    menu.style.display = "block"
+    tampa.style.display = "block"
+}
+function closemenu(){
+    menu.style.display = "none"
+    tampa.style.display = "none"
+}
