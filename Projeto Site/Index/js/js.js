a5b1bcaafa6ff2dcc6914e98d7663aa182000b67
Ju-Souza <ju.silva@bandtec.com.br>
2020-10-13 21:13:37 -0300

    One Page finalizada

--- Projeto Site/Index/js/js.js
+++ Projeto Site/Index/js/js.js
@@ -1,8 +1,14 @@
 function openmenu(){
     menu.style.display = "block"
     tampa.style.display = "block"
+    menu.classList.add("desce")
+    menu.classList.sobe("sobe")
 }
 function closemenu(){
-    menu.style.display = "none"
-    tampa.style.display = "none"
+    setTimeout(function(){
+        menu.style.display = "none"
+        tampa.style.display = "none"
+    }, 500)
+    menu.classList.add("sobe")
+    menu.classList.remove("desce")
 }
