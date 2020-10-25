6010c2b76dc3d58bb89d54591d8f17e99621c188
GVRO1 <gabriel.oliveira@bandte.com.br>
2020-10-25 21:21:25 -0200

    arrumando arquivos no css

--- nig/site/js/financeiro.js
+++ nig/site/js/financeiro.js
@@ -1,3 +1,5 @@
+var aberto_financeiro = false;
+
 function calculo_financeiro() {
     var area = Number(inp_area.value);
     var arduino = 100;
@@ -9,4 +11,27 @@ function calculo_financeiro() {
     resp.classList.add(`aparecer`)
     instalacao.innerHTML = `R$ ${instalar.toFixed(2)}`;
     quantid.innerHTML = Math.round(qtArduino + 0.4)
+}
+
+function calc_financeiro() {
+    if (!aberto_financeiro) {
+        tela_financeiro.classList.add(`abrir_financ`);
+        tela_financeiro.classList.remove(`fechar_financ`);
+        tela_financeiro.style.display = `block`
+        tampa_esquerda.classList.remove(`fechar_tampa_esquerda`);
+        tampa_esquerda.classList.add(`abrir_tampa_esquerda`);
+        tampa_esquerda.style.display = 'block'
+        aberto_financeiro = true
+    }
+    else {
+        tela_financeiro.classList.remove(`abrir_financ`);
+        tela_financeiro.classList.add(`fechar_financ`);
+        tampa_esquerda.classList.add(`fechar_tampa_esquerda`);
+        tampa_esquerda.classList.remove(`abrir_tampa_esquerda`);
+        setTimeout(function () {
+            tela_financeiro.style.display = `none`
+            tampa_esquerda.style.display = 'none'
+            aberto_financeiro = false
+        }, 1200)
+    }
 }
---
+++ nig/site/js/login.js
@@ -0,0 +1,34 @@
+var aberto_login = false;
+
+function logar() {
+    if (!aberto_login) {
+        tela_login.classList.add(`abrir_tela`);
+        tela_login.classList.remove(`fechar_tela`);
+        tampa.classList.add(`abrir_tampa`);
+        tampa.classList.remove(`fechar_tampa`);
+        tampa.classList.remove(`fechar_tampa_cadastro`);
+        tampa.classList.remove(`abrir_tampa_cadastro`);
+        tampa.style.display = 'block'
+        tela_login.style.display = `block`
+        aberto_login = true
+        tela_cadastro.classList.remove(`abrir_tela`);
+        tela_cadastro.classList.add(`fechar_tela`);
+        setTimeout(function () {
+            tela_cadastro.style.display = `none`
+            aberto_cadastro = false
+        }, 1200)
+    }
+    else if (aberto_login) {
+        tela_login.classList.remove(`abrir_tela`);
+        tela_login.classList.add(`fechar_tela`);
+        tampa.classList.add(`fechar_tampa`);
+        tampa.classList.remove(`abrir_tampa`);
+        tampa.classList.remove(`fechar_tampa_cadastro`);
+        tampa.classList.remove(`abrir_tampa_cadastro`);
+        setTimeout(function () {
+            tela_login.style.display = `none`
+            tampa.style.display = 'none'
+            aberto_login = false;
+        }, 500)
+    }
+}
