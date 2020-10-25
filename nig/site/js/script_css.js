2b19dc563d76303c14d3f7062169858f8978a0a9
Ju-Souza <ju.silva@bandtec.com.br>
2020-10-25 13:19:49 -0300

    insercao do login e do cadastro como pop-up

--- nig/site/js/script_css.js
+++ nig/site/js/script_css.js
@@ -1,14 +1,46 @@
-function openmenu(){
-    menu.style.display = "block"
-    tampa.style.display = "block"
-    menu.classList.add("desce")
-    menu.classList.sobe("sobe")
+var aberto_cadastro = false;
+var aberto_login = false;
+function cadastrar(){
+    if(!aberto_cadastro){
+        tela_cadastro.classList.add(`abrir_tela`);
+        tela_cadastro.classList.remove(`fechar_tela`);
+        tela_cadastro.style.display = `block`
+        aberto_cadastro = true
+        tela_login.classList.remove(`abrir_tela`);
+        tela_login.classList.add(`fechar_tela`);
+        setTimeout(function(){
+        tela_login.style.display = `none`
+        aberto_login = false;
+    }, 1200)
+    }
+    else{
+        tela_cadastro.classList.remove(`abrir_tela`);
+        tela_cadastro.classList.add(`fechar_tela`);
+        setTimeout(function(){
+            tela_cadastro.style.display = `none`
+            aberto_cadastro = false
+        }, 1200)
+    }
 }
-function closemenu(){
+function logar(){
+    if(!aberto_login){
+        tela_login.classList.add(`abrir_tela`);
+        tela_login.classList.remove(`fechar_tela`);
+        tela_login.style.display = `block`
+        aberto_login = true
+        tela_cadastro.classList.remove(`abrir_tela`);
+        tela_cadastro.classList.add(`fechar_tela`);
+        setTimeout(function(){
+            tela_cadastro.style.display = `none`
+            aberto_cadastro = false
+        }, 1200)
+    }
+    else if(aberto_login){
+    tela_login.classList.remove(`abrir_tela`);
+    tela_login.classList.add(`fechar_tela`);
     setTimeout(function(){
-        menu.style.display = "none"
-        tampa.style.display = "none"
-    }, 500)
-    menu.classList.add("sobe")
-    menu.classList.remove("desce")
+        tela_login.style.display = `none`
+        aberto_login = false;
+    }, 1200)
+    }
 }
